9b078365092b086d188a2e22d31bf6ce4925fed7
hari-hs <165835844+hari-hs@users.noreply.github.com>
2024-05-06 21:19:27 -0500

    Update TransactionHistoryRepository.java

--- Bank-Management/src/main/java/com/bank/management/TransactionHistory/TransactionHistoryRepository.java
+++ Bank-Management/src/main/java/com/bank/management/TransactionHistory/TransactionHistoryRepository.java
@@ -1,12 +1,9 @@
 package com.bank.management.TransactionHistory;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-import java.util.List;
 
-@Repository
 public interface TransactionHistoryRepository extends JpaRepository<TransactionHistory, Long> {
-    // Custom query method to find transaction histories by account ID
-    List<TransactionHistory> findByAccountId(Long accountId);
+
 }
+
