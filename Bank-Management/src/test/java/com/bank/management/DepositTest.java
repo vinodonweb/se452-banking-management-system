60fc923f24eeefd67a0142fd1c11a8223e7901f6
Syed Saifuddin <166879130+saifuddinsyed60@users.noreply.github.com>
2024-05-06 20:25:08 -0500

    Update DepositTest.java

--- Bank-Management/src/test/java/com/bank/management/DepositTest.java
+++ Bank-Management/src/test/java/com/bank/management/DepositTest.java
@@ -5,6 +5,7 @@ import com.bank.management.Deposit.DepositRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -12,12 +13,13 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
 @DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 public class DepositTest {
 
     @Autowired
     private DepositRepository depositRepo;
 
-    @DisplayName("Test composition in Lombok")
+    @DisplayName("Deposit Test")
     @Test
     public void testDeposit() {
         Deposit d = new Deposit();
@@ -34,26 +36,6 @@ public class DepositTest {
 
 
     }
-
-
-
-//    @DisplayName("Test adding flights")
-//    @Test
-//    public void testAddingPassengerAsComposition() {
-//        var person = new Person();
-//        person.setFirstName("Pat");
-//        person.setLastName("Sajak");
-//        var passenger = Passenger.builder().person(person).build();
-//
-//        var b4Add = passengerRepo.count();
-//        passengerRepo.save(passenger);
-//        var afterAdd = passengerRepo.count();
-//
-//        assertEquals(b4Add + 1, afterAdd);
-//
-//        var passengerList = passengerRepo.findByPersonLastName("Sajak");
-//        assertEquals(1, passengerList.size());
-//    }
 }
 
 
