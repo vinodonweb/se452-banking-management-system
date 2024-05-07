7f253f015c2084451a743c45530170e630837314
alnu59 <156729672+alnu59@users.noreply.github.com>
2024-05-06 22:46:28 -0500

    Added files for Settings
    Added files for settings for Customers Account.

---
+++ Bank-Management/src/main/java/com/bank/management/Settings/SettingsRepository.java
@@ -0,0 +1,7 @@
+package com.bank.management.Settings;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SettingsRepository extends JpaRepository<Settings, Long> {
+
+}
