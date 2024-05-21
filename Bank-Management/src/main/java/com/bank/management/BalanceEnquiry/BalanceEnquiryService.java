6abe022f75c84bd13b673448acd328e38bad2b9e
hari-hs <165835844+hari-hs@users.noreply.github.com>
2024-05-20 22:31:28 -0500

    Update BalanceEnquiryService.java

--- Bank-Management/src/main/java/com/bank/management/BalanceEnquiry/BalanceEnquiryService.java
+++ Bank-Management/src/main/java/com/bank/management/BalanceEnquiry/BalanceEnquiryService.java
@@ -1,4 +1,4 @@
-package com.bank.management.BalanceEnquiry;
+package com.bank.management.balanceenquiry;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
