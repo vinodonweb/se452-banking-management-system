7177afe45c95b3d5c562474278a6ecc024b0c1b3
hari-hs <165835844+hari-hs@users.noreply.github.com>
2024-05-20 22:32:46 -0500

    Update TransactionHistoryController.java

--- Bank-Management/src/main/java/com/bank/management/TransactionHistory/TransactionHistoryController.java
+++ Bank-Management/src/main/java/com/bank/management/TransactionHistory/TransactionHistoryController.java
@@ -1,12 +1,8 @@
-package com.bank.management.TransactionHistory;
+package com.bank.management.transactionhistory;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/transactions")
