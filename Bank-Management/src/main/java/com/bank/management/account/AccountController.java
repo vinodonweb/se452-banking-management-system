ac55d7e27a4b14dff135d3fff26a17eb950b62b9
Syed Saifuddin <166879130+saifuddinsyed60@users.noreply.github.com>
2024-05-21 13:01:08 -0500

    milestone 3

---
+++ Bank-Management/src/main/java/com/bank/management/account/AccountController.java
@@ -0,0 +1,45 @@
+package com.bank.management.account;
+
+import com.bank.management.ResponseAPI;
+import lombok.Data;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Data
+@Log4j2
+@RestController
+@RequestMapping("/api/account")
+@CrossOrigin(origins = "http://localhost:4200/")
+public class AccountController {
+
+    @Autowired
+    AccountService service;
+
+    @PostMapping
+    public ResponseAPI<Account> createAccount(@RequestBody Account a) {
+        ResponseAPI<Account> response=null;
+        boolean accountExists = service.existsByAccountNumber(a.getAccountNumber());
+        if (accountExists) {
+             response = new ResponseAPI<>(HttpStatus.BAD_REQUEST,
+                    "Uh-oh! It seems like there's already an account associated with the provided account number. Please verify your details and try again.", null);
+            log.error("exit deposit");
+            return response;
+        }
+        else {
+             response = new ResponseAPI<>(HttpStatus.CREATED, "Account created", service.save(a));
+
+        }
+        log.traceExit("exit deposit", a);
+        return response;
+    }
+
+    @GetMapping
+    public List<Account> showAccount()
+    {
+        return service.list();
+    }
+}
