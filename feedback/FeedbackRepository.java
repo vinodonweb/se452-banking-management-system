8f084e9fd6ecb50a11bca7cfea25de169d64f147
alnu59 <156729672+alnu59@users.noreply.github.com>
2024-05-27 20:29:59 -0500

    Delete feedback directory
    accidentally uploaded in wrong place

--- feedback/FeedbackRepository.java
+++
@@ -1,7 +0,0 @@
-package com.bank.management.feedback;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FeedbackRepository extends JpaRepository<Feedback, Long> {
-
-}
