e9e12a28c45d95947eea55b39f12d616927b26bc
Shlok Kadakia <77950374+skadakia01@users.noreply.github.com>
2024-05-27 19:11:56 -0500

    modified method

--- Bank-Management/src/main/java/com/bank/management/dashBoard/DashboardController.java
+++ Bank-Management/src/main/java/com/bank/management/dashBoard/DashboardController.java
@@ -1,20 +1,28 @@
 package com.bank.management.dashBoard;
 
+import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/dashboard")
+@Log4j2
+@CrossOrigin(origins = "http://localhost:4200/")
+
 public class DashboardController {
 
     @Autowired
     private DashboardService dashboardService;
 
-    @GetMapping("/data")
-    public Dashboard getDashboardData(@RequestParam Long accountId) {
-        return dashboardService.getDashboardData(accountId);
+//    @GetMapping
+//    public Dashboard getDashboardData(@RequestParam Long accountId) {
+//        return dashboardService.getDashboardData(accountId);
+//    }
+
+    @GetMapping
+    public List<Dashboard> getDashboard() {
+        return dashboardService.getDashboard();
     }
 }
