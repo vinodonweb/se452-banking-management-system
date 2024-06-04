f406b5cae907faf17d5e14047bf521c93047fad2
vinod sharma <vinodsharma80989@gmail.com>
2024-06-04 00:38:26 -0500

    Revert "backend implemented JWT and bcrypt"
    This reverts commit d75f1590b29b64ac398a7d2ad11ec7c237661c43.

--- Bank-Management/src/main/java/com/bank/management/login/LoginController.java
+++ Bank-Management/src/main/java/com/bank/management/login/LoginController.java
@@ -1,42 +1,31 @@
 package com.bank.management.login;
 
-import com.bank.management.*;
-import com.bank.management.security.*;
-import com.bank.management.signup.*;
-import lombok.extern.log4j.*;
-import org.springframework.beans.factory.annotation.*;
-import org.springframework.http.*;
-import org.springframework.security.authentication.*;
+import com.bank.management.ResponseAPI;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping(value = "/api/login", method = {RequestMethod.POST, RequestMethod.GET})
+@RequestMapping("/api/login")
 @Log4j2
 public class LoginController {
 
     @Autowired
     private LoginService loginService;
 
-    @Autowired
-    private AuthenticationManager manager;
-
-    @Autowired
-    private JwtHelper helper;
-
     @PostMapping("/authenticate")
-    public ResponseEntity<ResponseAPI<JWTResponse>> authenticate(@RequestBody Login login) {
+    public ResponseAPI<String> authenticate(@RequestBody Login login) {
         try {
-            Signup user = loginService.authenticate(login);
-            String token = helper.generateToken(user.getUsername());
-            return ResponseEntity.ok(new ResponseAPI<>(HttpStatus.OK, "Login successful", new JWTResponse(token, user.getUsername())));
+            loginService.authenticate(login);
+            return new ResponseAPI<>(HttpStatus.OK, "Login successful", null);
         } catch (LoginFailedException e) {
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(new ResponseAPI<>(HttpStatus.UNAUTHORIZED, e.getMessage(), null));
+            return new ResponseAPI<>(HttpStatus.UNAUTHORIZED, e.getMessage(), null);
         } catch (Exception e) {
             log.error("Unexpected error during user authentication", e);
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new ResponseAPI<>(HttpStatus.INTERNAL_SERVER_ERROR, "Error during login", null));
+            return new ResponseAPI<>(HttpStatus.INTERNAL_SERVER_ERROR, "Error during login", null);
         }
     }
-
 
     // Other login-related endpoints can be added here
 }
---
+++ Bank-Management/src/main/java/com/bank/management/login/LoginRepository.java
@@ -0,0 +1,8 @@
+//package com.bank.management.signup;
+//
+//import org.springframework.data.jpa.repository.JpaRepository;
+//
+//public interface SignupRepository extends JpaRepository<Signup, Long> {
+//    Signup findByUsername(String username);
+//    Signup findByEmail(String email);
+//}
