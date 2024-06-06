53703480abb4601a2a937b8014bde9928b497ab5
Syed Saifuddin <166879130+saifuddinsyed60@users.noreply.github.com>
2024-06-06 03:04:37 -0500

    Update TransactionHistoryController.java

--- Bank-Management/src/main/java/com/bank/management/transactionHistory/TransactionHistoryController.java
+++ Bank-Management/src/main/java/com/bank/management/transactionHistory/TransactionHistoryController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/transactions")
+@RequestMapping("/api/transactions")
 public class TransactionHistoryController {
 
     @Autowired
