862a70fb21bc4a0563a72c8e4886df5ac3e84f5a
Syed Saifuddin <166879130+saifuddinsyed60@users.noreply.github.com>
2024-06-06 12:50:45 -0500

    Update DepositService.java

--- Bank-Management/src/main/java/com/bank/management/deposit/DepositService.java
+++ Bank-Management/src/main/java/com/bank/management/deposit/DepositService.java
@@ -1,10 +1,15 @@
 package com.bank.management.deposit;
 
+import com.bank.management.transactionHistory.TransactionHistory;
+import com.bank.management.transactionHistory.TransactionHistoryRepository;
 import lombok.AllArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.sql.Time;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 @Service
@@ -14,6 +19,9 @@ public class DepositService {
 
     @Autowired
     private DepositRepository repo;
+
+    @Autowired
+    private TransactionHistoryRepository transactionRepo;
 
     public List<Deposit> list() {
         log.traceEntry("Enter list");
@@ -25,6 +33,8 @@ public class DepositService {
     public Deposit save(Deposit d) {
         log.traceEntry("enter save", d);
         log.traceExit("exit save", d);
+        TransactionHistory th = new TransactionHistory(d.getDepositId(),d.getTransactionNumber(), d.getAccountNumber(), d.getAmount(), "Deposit", LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")));
+        transactionRepo.save(th);
         return repo.save(d);
 
     }
@@ -37,10 +47,9 @@ public class DepositService {
     }
 
 
-
-        public Double sumDepositsByAccountId(Long accountId) {
-            return 5000.0;
-        }
+    public Double sumDepositsByAccountId(Long accountId) {
+        return 5000.0;
     }
+}
 
 
