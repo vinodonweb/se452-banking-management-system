471d661f2216d32342a2728a2a098b2ecc3d3fb5
Syed Saifuddin <166879130+saifuddinsyed60@users.noreply.github.com>
2024-06-08 08:12:18 -0500

    Update navbar.component.ts

--- frontend/src/app/components/navbar/navbar.component.ts
+++ frontend/src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { UiService } from 'src/app/services/ui.service';
 
 @Component({
   selector: 'app-navbar',
@@ -7,9 +10,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+  showLogOut:boolean;
+  private uiServiceSubscription: Subscription;
+
+  constructor(private uiService:UiService,private router:Router) { }
 
   ngOnInit(): void {
+    this.uiServiceSubscription = this.uiService.onToggleLogout().subscribe(show => {
+      this.showLogOut = show;
+    });
   }
+
+  public logout() {
+    localStorage.removeItem("loginUser");
+    this.uiService.setShowLogout(false);
+    this.router.navigate(['/login']);
+  }
+
 
 }
