605d05127b4810bf2a4cf426cbc0ba51a7c9663a
Syed Saifuddin <166879130+saifuddinsyed60@users.noreply.github.com>
2024-06-08 08:12:45 -0500

    Update signup.component.ts

--- frontend/src/app/components/signup/signup.component.ts
+++ frontend/src/app/components/signup/signup.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
+import { TaskService } from 'src/app/services/task.service';
+import { User } from 'src/app/User';
 
 @Component({
   selector: 'app-signup',
@@ -8,12 +11,35 @@ import { HttpClient } from '@angular/common/http';
 })
 export class SignupComponent implements OnInit {
 
-
-  constructor(private http: HttpClient) { }
+  signupObj: User = {
+    username: '',
+    password: '',
+    email: '',
+    accountType: ''
+  };
+  constructor(private http: HttpClient, private router: Router, private taskService: TaskService) { }
 
   ngOnInit(): void {
   }
 
+  onSignup() {
 
-  
+    //localStorage.setItem("signupUsers",JSON.stringify(this.signupUsers));
+    this.taskService.signup(this.signupObj).subscribe((resp: boolean) => {
+      if (resp)
+      {
+        debugger;
+        console.log(resp);
+        alert("User created successfully");
+      this.router.navigate(['/login']);
+      }
+      else {
+        alert("User/Email already exists")
+      }
+    }
+
+    )
+  }
+
+
 }
