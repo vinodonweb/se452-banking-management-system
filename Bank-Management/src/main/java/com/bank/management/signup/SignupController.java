3ae68b088d5a725dc8b6ff6d334f834cf8ea6898
Syed Saifuddin <166879130+saifuddinsyed60@users.noreply.github.com>
2024-06-08 07:20:13 -0500

    Update SignupController.java

--- Bank-Management/src/main/java/com/bank/management/signup/SignupController.java
+++ Bank-Management/src/main/java/com/bank/management/signup/SignupController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/api/signup")
 @Log4j2
+@CrossOrigin(origins = "http://localhost:4200/")
 public class SignupController {
     @Autowired
     private SignupService signupService;
