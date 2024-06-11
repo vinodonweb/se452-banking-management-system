1f198f42595889fffb87c6977d34278059026e58
Syed Saifuddin <166879130+saifuddinsyed60@users.noreply.github.com>
2024-06-11 17:44:41 -0500

    Fixed CORS issue

--- Bank-Management/src/main/java/com/bank/management/login/LoginController.java
+++ Bank-Management/src/main/java/com/bank/management/login/LoginController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/api/login")
 @Log4j2
-@CrossOrigin(origins = "http://localhost:4200/")
+@CrossOrigin(origins = "*") // Allow all origins
 public class LoginController {
 
     @Autowired
